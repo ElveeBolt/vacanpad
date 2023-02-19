828e2054346297dd529ce119c80f78f728312530
Elvee Bolt <boltelvee@gmail.com>
2023-02-19 12:27:38 +0200

    Added docker compose, alembic and Postgres connection

--- database.py
+++ database.py
@@ -1,9 +1,10 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from config import *
 
-engine = create_engine(f'sqlite:///{DATABASE}')
+DB_HOST = os.environ.get('DB_HOST', 'localhost')
+engine = create_engine(f'postgresql://postgres:password@{DB_HOST}:5432/postgres')
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
 Base = declarative_base()
